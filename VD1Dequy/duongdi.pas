7114bd67367da5f38fb6fa7d817caaa7f32457f0
Long Pham <longpham6595@gmail.com>
2020-09-18 01:10:41 +0700

    finish for day1

--- VD1Dequy/duongdi.pas
+++ VD1Dequy/duongdi.pas
@@ -1,99 +1,73 @@
-
-Program btmaudq;
-
-Uses crt;
-
-Var f: text;
-  n,a,b,r,s,csl: integer;
-  dd: array[1..100,1..100] Of boolean;
-  ck: array[1..100] Of boolean;
-  luuvet: array[1..100] Of integer;
-
-Procedure khoitao;
-
-Var p,q: integer;
-Begin
-  For p:=1 To 100 Do
-    For q:=1 To 100 Do
-      dd[p,q] := false;
-End;
-
-Procedure nhap;
-
-Var p: integer;
-Begin
+program btmaudq;
+ uses crt;
+ var f: text;
+ n,a,b,r,s,i,j,csl: integer;
+ dd: array[1..100,1..100] of boolean;
+ ck: array[1..100] of boolean;
+ luuvet: array[1..100] of integer;
+procedure khoitao;
+ var p,q: integer;
+ begin
+  for p:=1 to 100 do
+   for q:=1 to 100 do
+    dd[p,q]:=false;
+ end;
+procedure nhap;
+ var p,q:integer;
+ begin
   assign(f,'duongdi.inp');
   reset(f);
   readln(f,n,a,b);
-  While Not(eof(f)) Do
-    Begin
-      readln(f,r,s);
-      dd[r,s] := true;
-      dd[s,r] := true;
-    End;
+  while not(eof(f)) do
+   begin
+    readln(f,r,s);
+    dd[r,s]:=true;
+    dd[s,r]:=true;
+   end;
   close(f);
-  csl := 1;
-  luuvet[1] := a;
-  For p:=1 To 100 Do
-    ck[p] := false;
-  ck[a] := true;
-End;
-
-Procedure xuat;
-
-Var p: integer;
-Begin
-  For p:=1 To csl Do
-    write(luuvet[p],' ');
+  csl:=1;
+  luuvet[1]:=a;
+  for p:=1 to 100 do
+   ck[p]:=false;
+  ck[a]:=true;
+ end;
+procedure xuat;
+ var p:integer;
+ begin
+  for p:=1 to csl do
+   write(luuvet[p],' ');
   writeln;
-End;
-
-Procedure xuly(x:integer);
-
-Var p: integer;
-Begin
-  For p:=1 To n Do
-    Begin
-      If ((ck[p]=false) And (dd[x,p]=true) And (p<>b)) Then
-        Begin
-          inc(csl);
-          luuvet[csl] := p;
-          ck[p] := true;
-          xuly(p);
-          ck[p] := false;
-          luuvet[csl] := 0;
-          dec(csl);
-        End
-      Else
-        If ((p=b) And (dd[x,p]=true)) Then
-          Begin
-            inc(csl);
-            luuvet[csl] := p;
-            xuat;
-            luuvet[csl] := 0;
-            dec(csl);
-          End;
-    End;
-End;
-
-Procedure xuatthunghiem;
-
-Var k,p: integer;
-Begin
-  For k:=1 To n Do
-    Begin
-      For p:=1 To n Do
-        write(dd[k,p],' ');
-      writeln;
-    End;
-End;
-
-
-Begin
-  clrscr;
-  khoitao;
-  nhap;
-  xuly(a);
-  //xuatthunghiem;
-  readln;
-End.
+ end;
+procedure xuly(x:integer);
+ var p:integer;
+ begin
+  for p:=1 to n do
+   begin
+    if ((ck[p]=false) and (dd[x,p]=true) and (p<>b)) then
+     begin
+      inc(csl);
+      luuvet[csl]:=p;
+      ck[p]:=true;
+      xuly(p);
+      ck[p]:=false;
+      luuvet[csl]:=0;
+      dec(csl);
+     end
+    else
+     if ((p=b) and (dd[x,p]=true)) then
+      begin
+       inc(csl);
+       luuvet[csl]:=p;
+       xuat;
+       luuvet[csl]:=0;
+       dec(csl);
+      end;
+   end;
+ end;
+begin
+ clrscr;
+ khoitao;
+ nhap;
+ xuly(a);
+ readln;
+end.
